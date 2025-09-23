c7646d09c46c1258cab6ae8fef59fbc81a914e16
Hans Bakker <jjabakker@icloud.com>
2025-09-23 11:12:47 +0200

    Use the fat jar

--- paint-generate-squares/scripts/run_genererate_squares_app.sh
+++ paint-generate-squares/scripts/run_genererate_squares_app.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$SCRIPT_DIR/.."
 
 # Set classpath using the JARs in their respective target folders
-CLASSPATH="$PROJECT_ROOT/target/paint-generate-squares-1.0.0-SNAPSHOT.jar:$PROJECT_ROOT/../paint-shared-utils/target/paint-shared-utils-1.0.0-SNAPSHOT.jar"
+CLASSPATH="$PROJECT_ROOT/target/paint-generate-squares-1.0.0-SNAPSHOT-jar-with-dependencies.jar:$PROJECT_ROOT/../paint-shared-utils/target/paint-shared-utils-1.0.0-SNAPSHOT.jar"
 
 # Run the Java class
 java -cp "$CLASSPATH" generatesquares.GenerateSquares
