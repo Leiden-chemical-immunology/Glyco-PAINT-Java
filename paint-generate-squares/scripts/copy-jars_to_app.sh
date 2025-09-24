232259f110c5838860977c9544ae1d1684be2f6f
Hans Bakker <jjabakker@icloud.com>
2025-09-24 15:31:19 +0200

    Copy the fat jar

--- paint-generate-squares/scripts/copy-jars_to_app.sh
+++ paint-generate-squares/scripts/copy-jars_to_app.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Move to the project root
 PROJECT_ROOT="$SCRIPT_DIR/.."
 
-cp -v $PROJECT_ROOT/target/paint-generate-squares-1.0.0-SNAPSHOT.jar ~/Applications//Generate\ Squares.app/Contents/Java/
+cp -v $PROJECT_ROOT/target/paint-generate-squares-1.0.0-SNAPSHOT-jar-with-dependencies.jar ~/Applications//Generate\ Squares.app/Contents/Java/
 cp -v $PROJECT_ROOT/../paint-shared-utils/target/paint-shared-utils-1.0.0-SNAPSHOT.jar ~/Applications//Generate\ Squares.app/Contents/Java/
