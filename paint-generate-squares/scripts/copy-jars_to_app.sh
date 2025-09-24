78bb260dcb552079a38b39008037cc67c36f70c0
Hans Bakker <jjabakker@icloud.com>
2025-09-24 15:09:18 +0200

    New script to copy jars to Application

---
+++ paint-generate-squares/scripts/copy-jars_to_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Move to the project root
+PROJECT_ROOT="$SCRIPT_DIR/.."
+
+cp -v $PROJECT_ROOT/target/paint-generate-squares-1.0.0-SNAPSHOT.jar ~/Applications//Generate\ Squares.app/Contents/Java/
+cp -v $PROJECT_ROOT/../paint-shared-utils/target/paint-shared-utils-1.0.0-SNAPSHOT.jar ~/Applications//Generate\ Squares.app/Contents/Java/
