eceb69ffe1c6e91d1b8fcace5fe7353ffef42d6e
Hans Bakker <jjabakker@icloud.com>
2025-10-16 19:01:38 +0200

    New scripts

---
+++ Shell scripts/clean-all-paint-jars.sh
@@ -0,0 +1,4 @@
+find /Users/Hans/JavaPaintProjects -type f -name "*paint*.jar" -exec rm -v {} \;
+find /Users/Hans/.m2 -type f -name "*paint*.jar" -exec rm -v {} \;
+find /Applications/Fiji.App -type f -name "*paint*.jar" -exec rm -v {} \;
+find /Users/hans/Applications/Glyco-PAINT -type f -name "paint*.jar" -exec rm -v {} \;
