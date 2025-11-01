e64a8df326dc7c1bd220197bb229081551c830c5
Hans Bakker <jjabakker@icloud.com>
2025-11-01 07:23:52 +0100

    Align colons in background plots

--- paint-generate-squares/src/main/java/paint/generatesquares/calc/PlotUtils.java
+++ paint-generate-squares/src/main/java/paint/generatesquares/calc/PlotUtils.java
@@ -439,16 +439,16 @@ public class PlotUtils {
         g2.setColor(Color.WHITE);
         g2.fillRect(0, 0, w, h);
 
-        int marginLeft   = 70;
-        int marginBottom = 50;
-        int marginTop    = 50;
-        int marginRight  = 40;
+        int    marginLeft   = 70;
+        int    marginBottom = 50;
+        int    marginTop    = 50;
+        int    marginRight  = 40;
 
-        int binCount     = allBins.length;
-        int maxBinCount = Arrays.stream(allBins).max().orElse(1);
+        int    binCount     = allBins.length;
+        int    maxBinCount  = Arrays.stream(allBins).max().orElse(1);
 
-        double xStep  = (double)(w - marginLeft - marginRight) / binCount;
-        double yScale = (double)(h - marginTop - marginBottom) / maxBinCount;
+        double xStep        = (double)(w - marginLeft - marginRight) / binCount;
+        double yScale       = (double)(h - marginTop - marginBottom) / maxBinCount;
 
         g2.setColor(Color.GRAY);
         int x0 = marginLeft;
@@ -460,7 +460,7 @@ public class PlotUtils {
         int nTicks = 5;
         for (int i = 0; i <= nTicks; i++) {
             int y = y0 - (i * (h - marginTop - marginBottom) / nTicks);
-            int value = (int)Math.round(i * (double) maxBinCount / nTicks);
+            int value = (int) Math.round(i * (double) maxBinCount / nTicks);
             g2.drawLine(x0 - 5, y, x0, y);
             g2.drawString(String.valueOf(value), x0 - 45, y + 5);
         }
@@ -472,21 +472,21 @@ public class PlotUtils {
             int barW       = (int)(xStep * 0.8);
 
             if (background > 0) {
-                int bgHeight = (int)(background * yScale);
+                int bgHeight = (int) (background * yScale);
                 int bgY      = y0 - bgHeight;
-                g2.setColor(new Color(100,160,255,180));
+                g2.setColor(new Color(100, 160, 255, 180));
                 g2.fillRect(x, bgY, barW, bgHeight);
             }
 
-            int fgHeight = (int)((total - background) * yScale);
+            int fgHeight = (int) ((total - background) * yScale);
             if (fgHeight > 0) {
-                int fgY = y0 - fgHeight - (int)(background * yScale);
-                g2.setColor(new Color(180,180,180));
+                int fgY = y0 - fgHeight - (int) (background * yScale);
+                g2.setColor(new Color(180, 180, 180));
                 g2.fillRect(x, fgY, barW, fgHeight);
             }
 
             g2.setColor(Color.BLACK);
-            g2.drawRect(x, y0 - (int)(total * yScale), barW, (int)(total * yScale));
+            g2.drawRect(x, y0 - (int) (total * yScale), barW, (int) (total * yScale));
         }
 
         g2.setFont(new Font("SansSerif", Font.PLAIN, 12));
@@ -497,14 +497,45 @@ public class PlotUtils {
         g2.drawString("Track Count Histogram – " + title, w / 2 - 150, marginTop - 20);
 
         g2.setFont(new Font("SansSerif", Font.BOLD, 14));
-        g2.setColor(new Color(0,70,180));
-        int textX = w - 370;
-        int textY = marginTop + 20;
+        g2.setColor(new Color(0, 70, 180));
 
-        g2.drawString(String.format("Number of squares in recording             : %d", totalSquares),      textX, textY);
-        g2.drawString(String.format("Number of tracks in recording              : %d", totalTracks),     textX, textY + 20);
-        g2.drawString(String.format("Number of background squares               : %d", nBackground),      textX, textY + 40);
-        g2.drawString(String.format("Number of tracks in background             : %d", backgroundTracksTotal), textX, textY + 60);
-        g2.drawString(String.format("Average number of tracks in the background : %.3f", avgTracksInBackground), textX, textY + 80);
+        int textX = w - 400;
+        int textY = marginTop + 20;
+        int lineSpacing = 20;
+
+        String[] labels = new String[]{
+                "Number of squares in recording",
+                "Number of tracks in recording",
+                "Number of background squares",
+                "Number of tracks in background",
+                "Average number of tracks in the background"
+        };
+        String[] values = new String[]{
+                String.valueOf(totalSquares),
+                String.valueOf(totalTracks),
+                String.valueOf(nBackground),
+                String.valueOf(backgroundTracksTotal),
+                String.format("%.3f", avgTracksInBackground)
+        };
+
+        FontMetrics fm = g2.getFontMetrics();
+        int maxLabelW = 0;
+        for (String label : labels) {
+            maxLabelW = Math.max(maxLabelW, fm.stringWidth(label));
+        }
+
+        // Column positions
+        int colonPad = 10;                  // space between label end and colon
+        int valuePad = 10;                  // space between colon and value
+        int colonX = textX + maxLabelW + colonPad;
+        int valueX = colonX + fm.stringWidth(":") + valuePad;
+
+        // Draw rows with pixel-accurate alignment
+        for (int i = 0; i < labels.length; i++) {
+            int y = textY + i * lineSpacing;
+            g2.drawString(labels[i], textX, y);
+            g2.drawString(":", colonX, y);
+            g2.drawString(values[i], valueX, y);
+        }
     }
 }
