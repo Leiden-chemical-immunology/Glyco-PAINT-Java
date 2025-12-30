da7678c2b5496066bf3554a1da3d98329901dcbe
Hans Bakker <jjabakker@icloud.com>
2025-12-30 13:46:46 +0100

    Corrected slider control for R Squared: 0.1 -> 0.05

--- paint-viewer/src/main/java/paint/viewer/ui/dialogs/SquareControlDialog.java
+++ paint-viewer/src/main/java/paint/viewer/ui/dialogs/SquareControlDialog.java
@@ -313,7 +313,7 @@ public class SquareControlDialog extends JDialog {
     private void restoreOriginals() {
         densityRatioSlider.setValue((int) Math.round(origDensityRatio / 2.0));
         variabilitySlider.setValue((int) Math.round(origVariability / 1.0));
-        rSquaredSlider.setValue((int) Math.round(origRSquared / 0.1));
+        rSquaredSlider.setValue((int) Math.round(origRSquared / 0.05));
 
         switch (origNeighbourMode) {
             case "Relaxed":
